0f459764af1908fe0b1b84e74c6eda8d286fff52
Ethan Hair <ethan@astropanda.studio>
2024-01-08 12:38:59 -0600

    Added the GenericContextMenu

---
+++ src/Models/ContextMenuOpenedMessage.cs
@@ -0,0 +1,16 @@
+﻿namespace AstroPanda.Blazor.Toolkit.Models;
+
+public class ContextMenuOpenedMessage
+{
+    public ContextMenuOpenedMessage() { }
+
+    public ContextMenuOpenedMessage(Guid contextMenuId)
+    {
+        ContextMenuId = contextMenuId;
+    }
+
+    /// <summary>
+    /// The Id of the Context Menu that just opened
+    /// </summary>
+    public Guid ContextMenuId { get; set; }
+}
