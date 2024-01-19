e553b182d364aa2320bf2dc094c5cb622b684f28
Turner Bass <turner@astropanda.studio>
2024-01-19 12:34:18 -0600

    Add demo projects for WASM and Server rendering; Add Environment Label component

---
+++ src/AstroPanda.Blazor.Toolkit/Components/EnvironmentLabel.razor.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+#if NET8_0_OR_GREATER && BROWSER1_0_OR_GREATER 
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+#endif
+
+namespace AstroPanda.Blazor.Toolkit;
+public partial class EnvironmentLabel
+{
+#if NET8_0_OR_GREATER && BROWSER1_0_OR_GREATER 
+    [Inject]
+    private IWebAssemblyHostEnvironment _hostEnvironment { get; set; }
+#endif
+
+    [Parameter]
+    public List<string> HiddenEnvironments { get; set; } = new();
+
+    [Parameter]
+    public Color Color { get; set; } = Color.Error;
+
+    [Parameter]
+    public Size Size { get; set; } = Size.Medium;
+
+    private string _environment { get; set; } = string.Empty;
+
+    private bool _showLabel = false;
+
+
+    protected override async Task OnInitializedAsync()
+    {
+        _environment = GetEnvironment();
+        _showLabel = HiddenEnvironments.Contains(_environment) == false;
+    }
+
+    private string GetEnvironment()
+    {
+#if NET8_0_OR_GREATER && BROWSER1_0_OR_GREATER        
+        return _hostEnvironment.Environment;
+#else
+        return Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? string.Empty;
+#endif
+    }
+}
