f5bf7ad5aac4b088c434eac9acb601eb1f65fa8d
Ethan Hair <ethan@astropanda.studio>
2024-02-06 17:01:39 -0600

    Missed the custom attributes on the context menus

--- src/AstroPanda.Blazor.Toolkit/wwwroot/contextMenuService.js
+++ src/AstroPanda.Blazor.Toolkit/wwwroot/contextMenuService.js
@@ -1,15 +1,17 @@
 ﻿document.addEventListener("mousedown", (event) => {
     const contextMenuIds = [];
     const elementsUnderMouse = document.elementsFromPoint(event.clientX, event.clientY);
+    console.trace("*** The custom click listener was called");
 
     elementsUnderMouse.forEach(function (x) {
         if (!!x.getAttribute('context-menu-guid')) {
+            console.trace("*** Clicked on a context menu");
             contextMenuIds.push(x.getAttribute('context-menu-guid'));
         }
     });
 
     if (contextMenuIds.length !== 0) {
-        console.log("*** Attempting to call the CloseActiveContextMenu method from js")
+        console.trace("*** Attempting to call the CloseActiveContextMenu method from js");
         DotNet.invokeMethodAsync('AstroPanda.Blazor.Toolkit', 'CloseActiveContextMenu');
     }
 });
