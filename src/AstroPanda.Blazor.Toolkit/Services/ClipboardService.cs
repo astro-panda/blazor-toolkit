fe0a0808c466767a61ea160e7ae4eda09d1735be
Turner Bass <turner@astropanda.studio>
2024-03-09 08:02:14 -0600

    Add Clipboard Service EA-309

---
+++ src/AstroPanda.Blazor.Toolkit/Services/ClipboardService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.JSInterop;
+
+namespace AstroPanda.Blazor.Toolkit;
+
+public class ClipboardService : IClipboardService, IAsyncDisposable
+{
+    private readonly IJSRuntime _jsInterop;
+    private IJSObjectReference? module;    
+
+    public ClipboardService(IJSRuntime jsInterop, ISnackbar snackbar)
+    {
+        _jsInterop = jsInterop;        
+    }
+
+    public async Task CopyImage(string imageUrl)
+    {
+        module = await _jsInterop.InvokeAsync<IJSObjectReference>("import", "./clipboardService.js");
+        await module.InvokeVoidAsync("ClipboardService.copyRemoteImgToClipboard", imageUrl);
+    }
+
+    public async Task CopySVGImage(string svgElementId)
+    {
+        module = await _jsInterop.InvokeAsync<IJSObjectReference>("import", "./clipboardService.js");
+        await module.InvokeVoidAsync("ClipboardService.copySvgToClipboard", svgElementId);
+    }
+
+    public async Task CopyText(string text)
+    {
+        await _jsInterop.InvokeVoidAsync("navigator.clipboard.writeText", text);        
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (module is not null)
+        {
+            await module.DisposeAsync();
+        }
+    }
+}
