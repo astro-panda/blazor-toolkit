fe9cc7d8be5f0f02ec5ab9c2331b1af676a30ffb
Turner Bass <turner@astropanda.studio>
2024-03-20 16:48:54 -0500

    Updated Clipboard Service with correct javascript module wire up

--- src/AstroPanda.Blazor.Toolkit/Services/ClipboardService.cs
+++ src/AstroPanda.Blazor.Toolkit/Services/ClipboardService.cs
@@ -5,23 +5,24 @@ namespace AstroPanda.Blazor.Toolkit;
 public class ClipboardService : IClipboardService, IAsyncDisposable
 {
     private readonly IJSRuntime _jsInterop;
-    private IJSObjectReference? module;    
+    private readonly Lazy<Task<IJSObjectReference>> _embedClipBoardServiceTask;
 
     public ClipboardService(IJSRuntime jsInterop)
     {
-        _jsInterop = jsInterop;        
+        _jsInterop = jsInterop;
+        _embedClipBoardServiceTask = new(() => _jsInterop.InvokeAsync<IJSObjectReference>("import", "./_content/AstroPanda.Blazor.Toolkit/clipboardService.js").AsTask());
     }
 
     public async Task CopyImage(string imageUrl)
     {
-        module = await _jsInterop.InvokeAsync<IJSObjectReference>("import", "./clipboardService.js");
-        await module.InvokeVoidAsync("ClipboardService.copyRemoteImgToClipboard", imageUrl);
+        var clipBoardModule = await _embedClipBoardServiceTask.Value;
+        await clipBoardModule.InvokeVoidAsync("ClipboardService.copyRemoteImgToClipboard", imageUrl);
     }
 
     public async Task CopySVGImage(string svgElementId)
     {
-        module = await _jsInterop.InvokeAsync<IJSObjectReference>("import", "./clipboardService.js");
-        await module.InvokeVoidAsync("ClipboardService.copySvgToClipboard", svgElementId);
+        var clipBoardModule = await _embedClipBoardServiceTask.Value;
+        await clipBoardModule.InvokeVoidAsync("ClipboardService.copySvgToClipboard", svgElementId);
     }
 
     public async Task CopyText(string text)
@@ -31,8 +32,9 @@ public class ClipboardService : IClipboardService, IAsyncDisposable
 
     public async ValueTask DisposeAsync()
     {
-        if (module is not null)
+        if (_embedClipBoardServiceTask.IsValueCreated)
         {
+            var module = await _embedClipBoardServiceTask.Value;
             await module.DisposeAsync();
         }
     }
