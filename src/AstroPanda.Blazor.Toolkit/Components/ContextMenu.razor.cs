577fbdc51ec3e5a66e75c5f6578ceb8e3d2d5baa
Hope-Parnell <hope@astropanda.studio>
2024-10-24 16:08:54 -0500

    Add Context Menu service

---
+++ src/AstroPanda.Blazor.Toolkit/Components/ContextMenu.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace AstroPanda.Blazor.Toolkit.Components
+{
+    public partial class ContextMenu
+    {
+        [Parameter]
+        public RenderFragment Content { get; set; }
+
+        [Parameter]
+        public Guid Id { get; set; }
+
+        [Parameter]
+        public double X { get; set; }
+        [Parameter]
+        public double Y { get; set; }
+
+        [CascadingParameter]
+        private ContextMenuProvider Parent { get; set; }
+
+        public void SetCoordinates(Tuple<double, double> xy)
+        {
+            X = xy.Item1;
+            Y = xy.Item2;
+        }
+
+        public void Close()
+        {
+            Parent.CloseAll();
+        }
+    }
+}
